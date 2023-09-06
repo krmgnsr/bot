a76e7b0b9962a3d54cabe4dd0cb9280b56076874
krmgnsr <141348301+krmgnsr@users.noreply.github.com>
2023-09-06 14:02:25 +0300

    Update sifre.py

--- sifre.py
+++ sifre.py
@@ -1,11 +1,1 @@
-import random
 
-
-def sifre_uret(password):
-    karakter = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
-    sifre = ""
-    for i in range(password):
-        sifre += random.choice(karakter)
-
-    return sifre
